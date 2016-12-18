34a3201fd8861c64150a8446943a7416d59dadab
lgkahn <kahn@lgk.com>
2016-12-18 13:12:20 -0500

    version 3 live
    version 3 changes
    * Version 3:
    * Add option to control each light separately, so each gets its own
    color.
    * Add Holiday Mode option where you can individually control what colors
    are enabled.
    * Related, Add options to turn on/off each color.
    * Add every 1 minute option. Use Sparingly can overload back end
    Scheduling
    * Added better logging, so in notification window you can see current
    color set. This is in order to help debug for when lights stop
    responding.

--- gardenhue.groovy
+++ gardenhue.groovy
@@ -1,5 +1,5 @@
 /**
-* Garden Hue v 1.0 
+* Garden Hue v 3.0 
 * by lg. kahn
 *
 * This SmartApp will turn on if enabled after sunset and run to
@@ -15,9 +15,17 @@
 * Turn off all hues when saving app.
 *
 * Version 2.1:
-* add a switch/virtual switch option, which if selected the enable/disable of the app will follow.
+* Add a switch/virtual switch option, which if selected the enable/disable of the app will follow.
+*
+* Version 3:
+* Add option to control each light separately, so each gets its own color.
+* Add Holiday Mode option where you can individually control what colors are enabled.
+* Related, Add options to turn on/off each color.
+* Add every 1 minute option. Use Sparingly can overload back end Scheduling
+* Added better logging, so in notification window you can see current color set. This is in order to help debug for when lights stop responding.
 *
 * The latest version of this file can be found at
+*
 * https://github.com/lgkapps/SmartThingsPublic/gardenhue
 *
 */
@@ -42,7 +50,8 @@ preferences {
 
 	section("Choose cycle time between color changes? ") {
             input "cycletime", "enum", title: "Cycle time in minutes?" , options: [
-                "5",
+                                "1",
+                                "5",
 				"10", 
 				"15", 
 				"30", 
@@ -60,8 +69,32 @@ preferences {
         input "enabled","bool", title: "Enabled?", required: true, defaultValue: true
         input "enableswitch", "capability.switch", title: "Optional: Do you have a switch/virtual switch which the application enable/disable functionality should follow? If you do not want this feature leave blank.", multiple:false, required: false
         input "randomMode","bool", title: "Enable Random Mode?", required: true, defaultValue: false
+        input "individualControl","bool", title: "Control Each light Color Individually?", required: false, defaultValue: false
      }
     
+
+    section (" Colors Enabled") {
+        input "holidayMode","bool", title: "Allow Enable/Disable for Each Color/Holiday Mode?", required: false, defaultValue: false
+
+        input "redEnabled", "bool", title: "Enable Red?", required: false, defaultValue: true    
+        input "brickRedEnabled", "bool", title: "Enable Brick Red?", required: false, defaultValue: true    
+        input "safetyOrangeEnabled", "bool", title: "Enable Safety Orange?", required: false, defaultValue: true    
+        input "orangeEnabled", "bool", title: "Enable Orange?", required: false, defaultValue: true    
+        input "amberEnabled", "bool", title: "Enable Amber?", required: false, defaultValue: true    
+        input "yellowEnabled", "bool", title: "Enable Yellow?", required: false, defaultValue: true    
+        input "greenEnabled", "bool", title: "Enable Green?", required: false, defaultValue: true    
+        input "turquoiseEnabled", "bool", title: "Enable Turquoise?", required: false, defaultValue: true    
+        input "aquaEnabled", "bool", title: "Enable Aqua?", required: false, defaultValue: true    
+        input "navyBlueEnabled", "bool", title: "Enable Navy Blue?", required: false, defaultValue: true    
+        input "blueEnabled", "bool", title: "Enable Blue?", required: false, defaultValue: true    
+        input "indigoEnabled", "bool", title: "Enable Indigo?", required: false, defaultValue: true    
+        input "purpleEnabled", "bool", title: "Enable Purple?", required: false, defaultValue: true    
+        input "pinkEnabled", "bool", title: "Enable Pink?", required: false, defaultValue: true    
+        input "rasberryEnabled", "bool", title: "Enable Rasberry?", required: false, defaultValue: true    
+        input "whiteEnabled", "bool", title: "Enable White?", required: false, defaultValue: true    
+
+}
+
      section( "Notifications" ) {
         input("recipients", "contact", title: "Send notifications to") {
             input "sendPushMessage", "enum", title: "Send a push notification?", options: ["Yes", "No"], required: false
@@ -155,38 +188,43 @@ private def initialize() {
 // uses the run every instead of direct schedule to take load off of fixed times on hub
     switch (settings.cycletime)
     {
+     case "1":
+     log.debug "Switching color every minute."
+     schedule("1 * * * * ?",changeHandler)
+     break;
+
      case "5":
-     log.debug "switching color every 5 minutes."
+     log.debug "Switching color every 5 minutes."
      runEvery5Minutes(changeHandler)
      break;
       
      case "10":
-     log.debug "switching color every 10 minutes."
+     log.debug "Switching color every 10 minutes."
      runEvery10Minutes(changeHandler)
      break;
     
      case "15":
-     log.debug "switching color every 15 minutes."
+     log.debug "Switching color every 15 minutes."
      runEvery15Minutes(changeHandler)
      break;
     
      case "30":
-     log.debug "switching color every 30 minutes."
+     log.debug "Switching color every 30 minutes."
      runEvery30Minutes(changeHandler)
      break;
     
      case "1 hour":
-     log.debug "switching color every hour."
+     log.debug "Switching color every hour."
      runEvery1Hour(changeHandler)
      break;
     
      case  "3 hours":
-     log.debug "switching color every 3 hours"
+     log.debug "Switching color every 3 hours"
      runEvery3Hours(changeHandler)
      break;
       
      default:
-     log.debug "switching color every 30 minutes."
+     log.debug "Switching color every 30 minutes."
      runEvery30Minutes(changeHandler)
      break;
      
@@ -309,25 +347,112 @@ def changeHandler(evt) {
          
        log.debug "found $onstr that were on!"
     
-    if ((onHues.size() > 0) && (settings.enabled == true))
+    if ((numberon > 0) && (settings.enabled == true))
     {
       def newColor = ""
       if (settings.randomMode == true)
        {
+
+       // lgk define alternate color array if holiday mode enabled
+       if (settings.holidayMode == true)
+         {
+            log.debug "Holiday Mode = On, getting count of colors Enabled."
+            
+         // now define new array
+         def enabledColorArray = []
+
+           if (redEnabled == true)
+               enabledColorArray <<  "Red"
+           if (brickRedEnabled == true)
+               enabledColorArray <<  "Brick Red"
+           if (safetyOrangeEnabled == true)
+               enabledColorArray <<  "Safety Orange"
+           if (orangeEnabled == true)
+               enabledColorArray <<  "Orange"
+           if (amberEnabled == true)
+               enabledColorArray <<  "Amber"
+           if (yellowEnabled == true)
+               enabledColorArray <<  "Yellow"
+           if (greenEnabled == true)
+               enabledColorArray <<  "Green"
+           if (turquoiseEnabled == true)
+               enabledColorArray <<  "Turquoise"
+           if (aquaEnabled == true)
+               enabledColorArray <<  "Aqua"
+           if (navyBlueEnabled == true)
+               enabledColorArray <<  "Navy Blue"
+           if (blueEnabled == true)
+               enabledColorArray <<  "Blue"
+           if (indigoEnabled == true)
+               enabledColorArray <<  "Indigo"
+           if (purpleEnabled == true)
+               enabledColorArray <<  "Purple"
+           if (pinkEnabled == true)
+               enabledColorArray <<  "Pink"
+           if (rasberryEnabled == true)
+               enabledColorArray <<  "Rasberry"
+           if (whiteEnabled == true)
+               enabledColorArray <<  "White"
+
+
+
+           def int colorsEnabled = enabledColorArray.size
+           log.debug "Enabled Color Count = $colorsEnabled"
+           log.debug "Enabled Color Array = $enabledColorArray"
+
+           if ((numberon > 1) && (settings.individualControl = true))
+             {
+
+                 for (def i=0; i<numberon; i++) 
+                      {
+                            def int nextValue = new Random().nextInt(colorsEnabled)
+                            log.debug "Hue: $i, Random Number = $nextValue"
+                            newColor = enabledColorArray[nextValue]
+
+                            sendNotificationEvent("$app.label: Hue:$i - Setting Color to $newColor.")
+                            sendcolor(newColor,i)
+                     }
+             } // do each individually
+        
+           else
+               {
+
+                def int nextValue = new Random().nextInt(colorsEnabled)
+                log.debug "Random Number = $nextValue"
+                newColor = enabledColorArray[nextValue]
+
+                sendNotificationEvent("$app.label: Setting Color to $newColor.")
+                hues.on()
+                sendcolor(newColor)
+
+              }
+
+       } // holiday mode on
+
+
+     else {
+        // not holiday mode
+
         def int nextValue = new Random().nextInt(16)
         def colorArray = ["Red","Brick Red","Safety Orange","Orange","Amber","Yellow","Green","Turquoise","Aqua","Navy Blue","Blue","Indigo","Purple","Pink","Rasberry","White"]
-             
+
         log.debug "Random Number = $nextValue"
         newColor = colorArray[nextValue]    
+
+        sendNotificationEvent("$app.label: Setting Color to $newColor.")
+        hues.on()
+        sendcolor(newColor)
+  
+        }
+
        }
        
       else
       
       { // not random
       
-	  def currentColor = state.currentColor
-      
-    log.debug " in changeHandler got current color = $currentColor"
+       def currentColor = state.currentColor
+       log.debug " in changeHandler got current color = $currentColor"
 
        switch(currentColor) {
     
@@ -352,13 +477,13 @@ def changeHandler(evt) {
 		case "Green":
 			newColor = "Turquoise"
 			break;
-        case "Turquoise":
+                case "Turquoise":
 			newColor = "Aqua"
 			break;
 		case "Aqua":
 			newColor = "Navy Blue"
 			break;
-        case "Navy Blue":
+                case "Navy Blue":
 			newColor = "Blue"
 			break;
 		case "Blue":
@@ -367,37 +492,36 @@ def changeHandler(evt) {
 		case "Indigo":
 			newColor = "Purple"
 			break;
-        case "Purple":
+                case "Purple":
 			newColor = "Pink"
 			break;
-    	case "Pink":
+            	case "Pink":
 			newColor = "Rasberry"
 			break;
-        case "Rasberry":
+                case "Rasberry":
 			newColor = "White"
 			break;
-        case "White":
+                case "White":
 			newColor = "Red"
 			break;
-        default:
+                default:
         	//log.debug "in default"
-             newColor = "Red"
+                        newColor = "Red"
 			break;
 	}
-    } // end random or not
-    
-      log.debug "After Check new color = $newColor"
 
+      log.debug "After Check new color = $newColor"
       hues.on()
       sendcolor(newColor)
+
+    } // end random or not
+    
       }
    }
 }
 
 
-def sendcolor(color)
-{
-log.debug "In send color"
+def sendcolor(color) { log.debug "In send color"
 	//Initialize the hue and saturation
 	def hueColor = 0
 	def saturation = 100
@@ -487,6 +611,100 @@ log.debug "In send color"
 
 }
 
+
+def sendcolor(color,which)
+{
+        log.debug "In send color for hue $which"
+	//Initialize the hue and saturation
+	def hueColor = 0
+	def saturation = 100
+
+	//Use the user specified brightness level. If they exceeded the min or max values, overwrite the brightness with the actual min/max
+	if (brightnessLevel<1) {
+		brightnessLevel=1
+	}
+    else if (brightnessLevel>100) {
+		brightnessLevel=100
+	}
+
+	//Set the hue and saturation for the specified color.
+	switch(color) {
+		case "White":
+			hueColor = 0
+			saturation = 0
+			break;
+		case "Daylight":
+			hueColor = 53
+			saturation = 91
+			break;
+		case "Soft White":
+			hueColor = 23
+			saturation = 56
+			break;
+		case "Warm White":
+			hueColor = 20
+			saturation = 80 
+			break;
+        case "Navy Blue":
+            hueColor = 61
+            break;
+		case "Blue":
+			hueColor = 65
+			break;
+		case "Green":
+			hueColor = 33
+			break;
+        case "Turquoise":
+        	hueColor = 47
+            break;
+        case "Aqua":
+            hueColor = 50
+            break;
+        case "Amber":
+            hueColor = 13
+            break;
+		case "Yellow":
+			//hueColor = 25
+            hueColor = 17
+			break; 
+        case "Safety Orange":
+            hueColor = 7
+            break;
+		case "Orange":
+			hueColor = 10
+			break;
+        case "Indigo":
+            hueColor = 73
+            break;
+		case "Purple":
+			hueColor = 82
+			saturation = 100
+			break;
+		case "Pink":
+			hueColor = 90.78
+			saturation = 67.84
+			break;
+        case "Rasberry":
+            hueColor = 94
+            break;
+		case "Red":
+			hueColor = 0
+			break;
+         case "Brick Red":
+            hueColor = 4
+            break;                
+	}
+
+	//Change the color of the light
+	def newValue = [hue: hueColor, saturation: saturation, level: brightnessLevel]  
+	hues[which].setColor(newValue)
+        state.currentColor = color
+        mysend("$app.label: hue: $which, Setting Color = $color")
+        log.debug "$app.label: hue: $which Setting Color = $color"
+
+}
+
+
 private mysend(msg) {
 
     if (location.contactBookEnabled) {
@@ -507,4 +725,7 @@ private mysend(msg) {
 
    // log.debug msg
 }
+
+
+
 
